e4dea24aaa0bea6523408a56c29ed7a5dcc3b4fe
Yasaman <156900175+Ycmmn@users.noreply.github.com>
2025-08-09 04:50:43 +0000

    add src/tarin.py

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+#=========== import libraries =======
 from datasets import load_dataset
 from transformers import (
     T5TokenizerFast,
@@ -9,3 +10,17 @@ from transformers import (
 import evaluate
 import numpy as np
 import torch
+
+
+#========= Config Variables ==========
+MODEL_NAME = "t5-small"
+DATASET_NAME = "cnn_dailymail"
+DATASET_CONFIG = "3.0.0"
+MAX_INPUT_LENGTH = 512
+MAX_TARGET_LENGTH = 128
+BATCH_SIZE = 8
+EPOCHS = 1
+OUTPUT_DIR = "./t5_cnn_dailymail_finetuned"
+SEED = 42
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
